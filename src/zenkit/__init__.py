6b1c1b3d761034f34ee6be475722bdcb201fe78b
Luis Michaelis <me@lmichaelis.de>
2024-02-25 17:59:29 +0100

    feat(ModelAnimation): add `ModelAnimation` APIs

--- src/zenkit/__init__.py
+++ src/zenkit/__init__.py
@@ -13,11 +13,19 @@ __all__ = [
     "CutsceneMessage",
     "CutsceneLibrary",
     "CutsceneBlock",
+    "ModelAnimation",
+    "AnimationSample",
+    "Vec2f",
+    "Vec3f",
+    "Quat",
+    "AxisAlignedBoundingBox",
 ]
 
+from zenkit._core import Vec2f, Vec3f, Quat, AxisAlignedBoundingBox
 from zenkit.vfs import Vfs, VfsNode, VfsOverwriteBehavior
 from zenkit.log import LogLevel, set_logger, set_logger_default
 from zenkit.texture import Texture, TextureFormat
 from zenkit.stream import Read
 from zenkit.font import Font, FontGlyph
 from zenkit.cutscene import CutsceneBlock, CutsceneMessage, CutsceneLibrary
+from zenkit.animation import ModelAnimation, AnimationSample
--- src/zenkit/_core.py
+++ src/zenkit/_core.py
@@ -1,8 +1,9 @@
-__all__ = ["DLL", "Vec2f"]
+__all__ = ["DLL", "Vec2f", "Vec3f", "Quat", "AxisAlignedBoundingBox", "Date"]
 
 import atexit
 import functools
-from ctypes import CDLL, c_float, Structure
+from ctypes import CDLL, c_float, Structure, c_uint32, c_uint16
+from datetime import datetime
 from pathlib import Path
 from typing import Final
 
@@ -10,6 +11,31 @@ _PATH = Path(__file__).parent / "native" / "linux-x64.so"
 DLL: Final[CDLL] = CDLL(str(_PATH))
 
 atexit.register(functools.partial(print, DLL))
+
+
+class Date(Structure):
+    _pack_ = 1
+    _fields_ = [
+        ("year", c_uint32),
+        ("month", c_uint16),
+        ("day", c_uint16),
+        ("hour", c_uint16),
+        ("minute", c_uint16),
+        ("second", c_uint16),
+    ]
+
+    def to_datetime(self) -> datetime:
+        return datetime(
+            year=self.year,
+            month=self.month,
+            day=self.day,
+            hour=self.hour,
+            minute=self.minute,
+            second=self.second,
+        )
+
+    def __repr__(self) -> str:
+        return f"Date(year={self.year}, month={self.month}, day={self.day}, hour={self.hour}, minute={self.minute}, second={self.second})"
 
 
 class Vec2f(Structure):
@@ -20,3 +46,36 @@ class Vec2f(Structure):
 
     def __repr__(self) -> str:
         return f"Vec2f(x={self.x}, y={self.y})"
+
+
+class Vec3f(Structure):
+    _fields_ = [
+        ("x", c_float),
+        ("y", c_float),
+        ("z", c_float),
+    ]
+
+    def __repr__(self) -> str:
+        return f"Vec3f(x={self.x}, y={self.y}, z={self.z})"
+
+
+class Quat(Structure):
+    _fields_ = [
+        ("x", c_float),
+        ("y", c_float),
+        ("z", c_float),
+        ("w", c_float),
+    ]
+
+    def __repr__(self) -> str:
+        return f"Quat(x={self.x}, y={self.y}, z={self.z}, w={self.w})"
+
+
+class AxisAlignedBoundingBox(Structure):
+    _fields_ = [
+        ("min", Vec3f),
+        ("max", Vec3f),
+    ]
+
+    def __repr__(self) -> str:
+        return f"AxisAlignedBoundingBox(min={self.min}, max={self.max})"
