e631995c154647cbd4fb87e5a140cb85dc9e205c
Kamil Krzyśków <kamilzary@gmail.com>
2024-11-12 00:47:53 +0100

    feat: add __str__ for symbol

--- src/zenkit/daedalus_script.py
+++ src/zenkit/daedalus_script.py
@@ -231,6 +231,9 @@ class DaedalusSymbol:
     def __repr__(self) -> str:
         return f"<{self.__class__.__name__} handle={self._handle} name={self.name!r} type={self.type.name}>"
 
+    def __str__(self) -> str:
+        return str(self.value) if self.value is not None else self.name
+
 
 class DaedalusInstruction(Structure):
     _fields_: ClassVar[tuple[str, Any]] = [
